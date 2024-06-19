26c5dac2fa8ae8afcb2f59f687ad3c9599bfbbc2
MCS <mcs@gmail.com>
2024-06-19 19:34:30 +0300

    added base web server with a few demo routes

--- app/main.go
+++ app/main.go
@@ -1,9 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("accessing index")
+	})
+
+	router.HandleFunc("POST /create", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("creating dynamic link")
+	})
+
+	router.HandleFunc("GET /link/{id}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("creating dynamic link")
+	})
+
+	router.HandleFunc("DELETE /link/{id}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("deleting dynamic link")
+	})
 
 	http.ListenAndServe(":9000", router)
 }
